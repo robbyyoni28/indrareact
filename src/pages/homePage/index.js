446dce7dd425ba667e58c110f4dec09d209c05c3
robbyyoni28 <robbyfebrianursaleh@gmail.com>
2022-08-29 20:14:16 +0800

    gaskan

--- src/pages/homePage/index.js
+++ src/pages/homePage/index.js
@@ -1,8 +1,31 @@
-import { StyleSheet, Text, View, Image } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Image, Card } from 'react-native'
+import React, {useEffect} from 'react'
 import { logoBeonWhite } from '../../assets'
+import axios from 'axios'
+import { FlatList } from 'react-native-gesture-handler'
 
 const homePage = () => {
+  const getInfluencer = async() =>{
+  var axios = require('axios');
+
+var config = {
+  method: 'get',
+  url: 'http://165.22.242.149:8080/influencer',
+  headers: { }
+};
+
+axios(config)
+.then(function (response) {
+  console.log(JSON.stringify(response.data.userdata));
+})
+.catch(function (error) {
+  console.log(error);
+});
+}
+useEffect(() => {
+  getInfluencer()
+},[])
+
   return (
     <View style={styles.container}>
     <View style={styles.box}>
@@ -11,6 +34,7 @@ const homePage = () => {
     <Text style={ styles.marketing }>Marketing</Text>
     <Text style={ styles.platform }>Platform</Text>
     </View>
+    <FlatList numColumns={2} ><Card /></FlatList>
     </View>
   )
 }
